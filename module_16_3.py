537197c2d38e00ceaa59e207c0b6e95975228f91
EugeneSP <epoltavets@gmail.com>
2024-09-14 17:39:38 +0300

    First commit

---
+++ module_16_3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# from fastapi import FastAPI, status, Body, HTTPException
+from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+# from typing import List
+from typing import Annotated
+
+# ...$ python3 -m uvicorn module_16_3:app
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+# class Message(BaseModel):
+#     id: int = None
+#     text: str
+
+@app.get('/users')
+async def get_users() -> dict:
+    return users
+
+@app.get('/user/{username}/{age}')
+# @app.get('/user/{username}')
+def post_user(username: str, age: int) -> str:
+# def post_user(username: str) -> str:
+    print('post user', username)
+    user_keys = [int(i) for i in users.keys()]
+    user_id = max(user_keys) + 1
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+@app.put('/user/{user_id}/{username}/{age}')
+def update_user(user_id: int, username: str, age: int) -> str:
+    try:
+        users[user_id] = f'Имя: {username}, возраст: {age}'
+        return f'User {user_id} is registered'
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f'User {user_id} not found')
+
+@app.delete('/user/{user_id}')
+def delete_user(user_id: int) -> str:
+    try:
+        users.pop(user_id)
+        return f'User {user_id} has been deleted'
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f'User {user_id} not found')
+
+
+
