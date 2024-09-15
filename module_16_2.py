c85d0c2a66dd068053d94582394db547c87af7ce
EugeneSPb1956 <75947698+EugeneSPb1956@users.noreply.github.com>
2024-09-15 09:53:41 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/user/admin')
+async def admin_page() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+@app.get('/user/{user_id}')
+async def user_id_page(user_id: Annotated[int, Path(de=0, ge=1, le=100, description='Enter User ID', example='123')]) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+@app.get('/user/{username}/{age}')
+async def user(username: Annotated[
+    str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+            age: Annotated[int, Path(de=0, ge=18, le=120, description='Enter age', example='24')]) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+
+@app.get('/')
+async def main_page() -> dict:
+    return {'message': 'Main page'}
+
