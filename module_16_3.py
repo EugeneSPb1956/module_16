925bc2de0d0e9558b8a5aa5733442fa6d149e9b5
EugeneSP <epoltavets@gmail.com>
2024-09-23 11:36:45 +0300

    Pip 8 fixes for module_16_2.py and module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -2,9 +2,6 @@
 
 # from fastapi import FastAPI, status, Body, HTTPException
 from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# from typing import List
-from typing import Annotated
 
 # ...$ python3 -m uvicorn module_16_3:app
 
@@ -12,23 +9,20 @@ app = FastAPI()
 
 users = {'1': 'Имя: Example, возраст: 18'}
 
-# class Message(BaseModel):
-#     id: int = None
-#     text: str
 
 @app.get('/users')
 async def get_users() -> dict:
     return users
 
+
 @app.get('/user/{username}/{age}')
-# @app.get('/user/{username}')
 def post_user(username: str, age: int) -> str:
-# def post_user(username: str) -> str:
     print('post user', username)
     user_keys = [int(i) for i in users.keys()]
     user_id = max(user_keys) + 1
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} is registered'
+
 
 @app.put('/user/{user_id}/{username}/{age}')
 def update_user(user_id: int, username: str, age: int) -> str:
@@ -38,6 +32,7 @@ def update_user(user_id: int, username: str, age: int) -> str:
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User {user_id} not found')
 
+
 @app.delete('/user/{user_id}')
 def delete_user(user_id: int) -> str:
     try:
@@ -45,6 +40,3 @@ def delete_user(user_id: int) -> str:
         return f'User {user_id} has been deleted'
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User {user_id} not found')
-
-
-
