b12bae90cfb1e442151d08e8aed38ec4d4ac1d48
EugeneSP <epoltavets@gmail.com>
2024-09-24 15:35:33 +0300

    Fixes on @app.post and @app.delete

--- module_16_3.py
+++ module_16_3.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from fastapi import FastAPI, status, Body, HTTPException
 from fastapi import FastAPI, HTTPException
-
-# ...$ python3 -m uvicorn module_16_3:app
 
 app = FastAPI()
 
@@ -11,13 +8,12 @@ users = {'1': 'Имя: Example, возраст: 18'}
 
 
 @app.get('/users')
-async def get_users() -> dict:
+def get_users() -> dict:
     return users
 
 
-@app.get('/user/{username}/{age}')
+@app.post('/user/{username}/{age}')
 def post_user(username: str, age: int) -> str:
-    print('post user', username)
     user_keys = [int(i) for i in users.keys()]
     user_id = max(user_keys) + 1
     users[user_id] = f'Имя: {username}, возраст: {age}'
@@ -28,15 +24,18 @@ def post_user(username: str, age: int) -> str:
 def update_user(user_id: int, username: str, age: int) -> str:
     try:
         users[user_id] = f'Имя: {username}, возраст: {age}'
-        return f'User {user_id} is registered'
+        return f'User {user_id} is updated'
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User {user_id} not found')
 
 
 @app.delete('/user/{user_id}')
-def delete_user(user_id: int) -> str:
+def delete_user(user_id: str) -> str:
+    print(user_id, type(user_id))
     try:
         users.pop(user_id)
         return f'User {user_id} has been deleted'
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User {user_id} not found')
+
+# ...$ python3 -m uvicorn module_16_3:app
